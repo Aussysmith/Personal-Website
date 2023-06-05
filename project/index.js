defe43eb4a95d3c7418076f2e01b94f71dad4e7f
Josh Traill <joshdtraill@gmail.com>
2023-06-04 20:44:40 -0700

    refactor: if statements refactor: general clean

--- project/index.js
+++ project/index.js
@@ -3,14 +3,13 @@ let count = localStorage.getItem("count");
 
 function Counter() {
     const isIndexPage = window.location.pathname.endsWith("index.html");
-    if (isIndexPage) {
-        count++;
-        localStorage.setItem("count", count);
-    }
+    if(!isIndexPage) return;
+
+    count++;
+    localStorage.setItem("count", count);
+    
     const countElement = document.getElementById("count");
-    if (countElement) {
-        countElement.innerHTML = `You have visited my website ${count} times.`;
-    }
+    countElement.innerHTML = `You have visited my website ${count} times.`;
 }
 
 window.onload = function() {
